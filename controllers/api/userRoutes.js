ebc183a2d890ab5629e4078ddfaa43b84d92ab34
Ash <chibane.tkd04@gmail.com>
2023-03-01 12:16:42 -0700

    added codes to Routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { User, Project } = require('../models');
+const ChckAuth = require('../utils/auth');
+
+// Render the login page
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// Authenticate the user and log them in
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Log the user out and redirect to the homepage
+router.get('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+// Render the signup page
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+// Create a new user account
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
