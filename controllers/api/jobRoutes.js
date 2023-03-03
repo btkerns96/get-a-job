55a69d3f6a950a29852dfc03150fd18dc231be4d
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-03-02 17:11:55 -1000

    connecting to localhost

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -1,49 +1,62 @@
 const router = require('express').Router();
-const { JobPosts, User} = require('../../models');
-const ChckAuth = require('../utils/auth');
+const { JobPosts } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Get all job posts and include user 
-router.get('/jobs', async (req, res) => {
-  try {
-    const jobData = await JobPosts.findAll({
-      include: [{ model: User }],
-    });
-    const jobs = jobData.map((job) => job.get({ plain: true }));
-    res.render('jobs', { jobs });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/jobs', async (req, res) => {
+//   try {
+//     const jobData = await JobPosts.findAll({
+//       include: [{ model: User }],
+//     });
+//     const jobs = jobData.map((job) => job.get({ plain: true }));
+//     res.render('jobs', { jobs });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// Get a single job post by id and include user 
-router.get('/jobs/:id', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
-    const jobData = await JobPosts.findByPk(req.params.id, {
-      include: [{ model: User }],
-    });
-    const job = jobData.get({ plain: true });
-    res.render('job', { job });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Render the new job post form
-router.get('/jobs/new', ChckAuth, (req, res) => {
-  res.render('new-job');
-});
-
-// Create a new job post
-router.post('/new', ChckAuth, async (req, res) => {
-  try {
-    const jobData = await JobPosts.create({
+    const jobPosts = await JobPosts.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-    res.status(200).json(jobData);
+
+    res.status(200).json(jobPosts);
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+// Get a single job post by id and include user 
+// router.get('/jobs/:id', async (req, res) => {
+//   try {
+//     const jobData = await JobPosts.findByPk(req.params.id, {
+//       include: [{ model: User }],
+//     });
+//     const job = jobData.get({ plain: true });
+//     res.render('job', { job });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// Render the new job post form
+// router.get('/jobs/new', ChckAuth, (req, res) => {
+//   res.render('new-job');
+// });
+
+// Create a new job post
+// router.post('/new', ChckAuth, async (req, res) => {
+//   try {
+//     const jobData = await JobPosts.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+//     res.status(200).json(jobData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,69 +1,147 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User } = require('../../models');
 
-// Render the login page
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
+router.post('/', async (req, res) => {
+  try {
+      const userData = await User.create(req.body);
+
+      req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+
+          res.status(200).json(userData);
+      });
+  } catch (err) {
+      res.status(400).json(err);
   }
-
-  res.render('login');
 });
 
-// Authenticate the user and log them in
+router.post('/', async (req, res) => {
+  try {
+      const userData = await User.create(req.body);
+
+      req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+
+          res.status(200).json(userData);
+      });
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-    if (!userData) {
-      res.status(400).json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
+      const userData = await User.findOne({ where: { email: req.body.email } });
 
-    const validPassword = userData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
+      if (!userData) {
+          res
+              .status(400)
+              .json({ message: 'Incorrect username or password, please try again' });
+          return;
+      }
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      res.status(200).json({ user: userData, message: 'You are now logged in!' });
-    });
+      const validPassword = await userData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+          res
+              .status(400)
+              .json({ message: 'Incorrect username or password, please try again' });
+          return;
+      }
+
+      req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+
+          res.json({ user: userData, message: 'You are now logged in!' });
+      });
+
   } catch (err) {
-    res.status(400).json(err);
+      res.status(400).json(err);
   }
 });
 
-// Log the user out and redirect to the homepage
-router.get('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.redirect('/');
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-// Render the signup page
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-// Create a new user account
 router.post('/signup', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+    const newSignup = await User.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
+
+    res.status(200).json(newSignup);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// router.post('/logout', (req, res) => {
+//   if (req.session.logged_in) {
+//       req.session.destroy(() => {
+//           res.status(204).end();
+//       });
+//   } else {
+//       res.status(404).end();
+//   }
+// });
+
 module.exports = router;
+
+
+// Authenticate the user and log them in
+// router.post('/login', async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ where: { email: req.body.email } });
+//     if (!userData) {
+//       res.status(400).json({ message: 'Incorrect email or password, please try again' });
+//       return;
+//     }
+
+//     const validPassword = userData.checkPassword(req.body.password);
+//     if (!validPassword) {
+//       res.status(400).json({ message: 'Incorrect email or password, please try again' });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
+//       res.status(200).json({ user: userData, message: 'You are now logged in!' });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// Log the user out and redirect to the homepage
+// router.get('/logout', (req, res) => {
+//   if (req.session.logged_in) {
+//     req.session.destroy(() => {
+//       res.redirect('/');
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+
+// Render the signup page
+// router.get('/signup', (req, res) => {
+//   res.render('signup');
+// });
+
+// Create a new user account
+// router.post('/signup', async (req, res) => {
+//   try {
+//     const userData = await User.create(req.body);
+//     req.session.save(() => {
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
+//       res.status(200).json({ user: userData, message: 'You are now logged in!' });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// module.exports = router;
